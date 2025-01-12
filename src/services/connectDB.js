3ef3bf6588790acf2c6b10b0fce2105142a5c06d
azalea <alissaperalta@outlook.com>
2025-01-12 22:37:58 +0800

    add database sequelize connection

---
+++ src/services/connectDB.js
@@ -0,0 +1,18 @@
+import { DB } from "../database/index.js";
+import sequelize from "../database/config/sequelize.js";
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Successfully connected to the database");
+
+    await DB.sequelize.sync({
+      force: false,
+    });
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
