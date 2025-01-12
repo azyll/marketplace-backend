048dfe60fae169665f65358b4830fbe5c2fd07e1
azalea <alissaperalta@outlook.com>
2025-01-12 22:37:31 +0800

    connect database to server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import http from "http";
 import app from "./app.js";
+import connectDB from "./services/connectDB.js";
 
 const PORT = 3000;
 
 const server = http.createServer(app);
 
-const startServer = () => {
+const startServer = async () => {
+  await connectDB();
+
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
 
-startServer();
+await startServer();
