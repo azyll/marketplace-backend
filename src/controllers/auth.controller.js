d257988958ffa90cdb54b2163ee9f219f79d5fd2
azalea <alissaperalta@outlook.com>
2025-08-05 00:08:01 +0800

    fix product category filtering

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,16 +5,20 @@ import {defaultErrorMessage} from '../utils/error-message.js';
 export const login = async (req, res) => {
   try {
     const {email, password} = req.body;
+
     const accessToken = await AuthService.login(email, password);
+
     return res.status(200).json({accessToken});
   } catch (err) {
     const message = 'Failed to login';
+
     if (err instanceof UnauthorizedException) {
       return res.status(err.statusCode).json({
         message,
         error: err.message
       });
     }
+
     return res.status(400).json({message, error: error.message || defaultErrorMessage});
   }
 };
