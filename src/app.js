4f064f4b0a241cd9e000483aedbf9aa0e3db0386
azalea <alissaperalta@outlook.com>
2025-08-19 13:56:23 +0800

    fix cors: update config for frontend

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://sti-marketplace.netlify.app', 'https://sti-fairview-proware.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api', routes);
